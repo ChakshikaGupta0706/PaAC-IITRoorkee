6d7f7743f8ba9c1eb023340502ea08d5fb5dcea0
ChakshikaGupta0706 <chakshikagupta@gmail.com>
2025-02-03 05:49:11 +0530

    Update paac.js

--- paac.js
+++ paac.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const swiper = new Swiper('.events-slider', {
 
-        slidesPerView: 1,
+        slidesPerView: 1.8,
         centeredSlides: true,
-        spaceBetween: 30,
+        spaceBetween: 70,
+
         mousewheel: {
             enabled: true,
-            sensitivity: 1
+            sensitivity: 1,
         },
 
         keyboard: {
@@ -23,13 +24,37 @@ document.addEventListener('DOMContentLoaded', function() {
             prevEl: '.swiper-button-prev',
         },
 
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        },
+        speed: 800,
+        effect: 'slide',
 
         loop: true,
-        
-    })
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        },
 
-})
+        breakpoints: {
+            1400: {
+                slidesPerView: 1.8,
+                spaceBetween: 100
+            },
+            
+            1200: {
+                slidesPerView: 1.6,
+                spaceBetween: 80
+            },
+            768: {
+                slidesPerView: 1.4,
+                spaceBetween: 60
+            },
+
+            480: {
+                slidesPerView: 1.4,
+                spaceBetween: 30
+            }
+        }
+        
+    });
+
+});
